104ffc22667793f9075763cfc12a82632adf53f6
Arpit <arpitbindal96@gmail.com>
2022-04-14 21:25:55 +0530

    initial commit

--- src/pages/adminPanel/Gateway/Gateway.jsx
+++ src/pages/adminPanel/Gateway/Gateway.jsx
@@ -2,10 +2,14 @@ import axios from 'axios';
 import React, { useEffect, useRef, useState } from 'react';
 import Overlay from '../../../containers/Overlay/Overlay';
 import { baseUrl } from '../../../utils/config/urls';
-import { getAddress, getSignMessage } from '../../../utils/services/auth';
+import { getAddress } from '../../../utils/services/auth';
 import { notify } from '../../../utils/services/notification';
-import { transferUsdc } from '../../../utils/services/transferUSDC';
+import { SendTransaction } from '../../../utils/services/transferUSDC';
 import './Gateway.scss'
+import { useMoralis, useWeb3Transfer } from "react-moralis";
+
+
+
 
 
 const getData = async (setSubdomain) => {
@@ -19,20 +23,26 @@ const getData = async (setSubdomain) => {
 
 }
 
-const createGateway = async (value) => {
-    await transferUsdc();
-    if (value.length > 0) {
-        axios.post(`${baseUrl}/api/gateway/add_subdomain`, {
-            "publicKey": getAddress(),
-            "subDomain": value,
-            "signedMessage": getSignMessage()
-        }).then((res) => {
-            console.log(res);
-        })
+const createGateway = async () => {
+    SendTransaction()
+    // console.log(Moralis.Moralis.Units.Token(20, 18));
+    // console.log(Moralis);
 
-    } else {
-        notify('Enter Custom Gateway Domain', 'error')
-    }
+    // const { fetch, error, isFetching } = Web3Transfer();
+    // console.log(fetch, error, isFetching);
+    // await transferUsdc();
+    // if (value.length > 0) {
+    //     axios.post(`${baseUrl}/api/gateway/add_subdomain`, {
+    //         "publicKey": getAddress(),
+    //         "subDomain": value,
+    //         "signedMessage": getSignMessage()
+    //     }).then((res) => {
+    //         console.log(res);
+    //     })
+
+    // } else {
+    //     notify('Enter Custom Gateway Domain', 'error')
+    // }
 
 
 }
@@ -41,16 +51,39 @@ const createGateway = async (value) => {
 function Gateway() {
     const [subdomain, setSubdomain] = useState(null);
     const inputRef = useRef(null)
+    const { Moralis } = useMoralis();
+
+    const params = {
+        amount: Moralis.Units.Token(1, 18),
+        receiver: "0x420B7C7114F7372207Ab0b36F1353B5d2D3b2afB",
+        type: "erc20",
+        contractAddress: "0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2",
+    }
+    console.log(params);
+
+    const { fetch, error, isFetching } = useWeb3Transfer(params);
 
     useEffect(() => {
         getData(setSubdomain);
-
         return () => {
         }
     }, [])
 
+    const MoralisTemp = async () => {
+        // console.log(Moralis.Moralis.Units.Token(20, 18));
+        // console.log(Moralis);
+        // Moralis.authenticate();
+        // const options = {
+        //     type: "erc20",
+        //     amount: Moralis.Moralis.Units.Token("0.5", "18"),
+        //     receiver: "0x..",
+        //     contractAddress: "0x..",
+        // };
+        // let result = await Moralis.Moralis.transfer(options);
+    };
+
     return <div className="gateway">
-        <Overlay />
+        {/* <Overlay /> */}
         <div className="gateway__title">
             <p>Gateway</p>
         </div>
@@ -58,8 +91,12 @@ function Gateway() {
         <div className="gateway__content">
             <p>Add Subdoamin at lighthouse.storage</p>
             <input type="text" placeholder='Enter Subdomain' value={subdomain} ref={inputRef} />
+            {/* {error && <p>
+                Error - {error}
+            </p>} */}
             <button className="btn" onClick={() => {
-                createGateway(inputRef.current.value)
+                createGateway()
+                // createGateway(inputRef.current.value, Moralis, Web3Tra/nsfer)
             }}>
                 {
                     subdomain ? <span>
--- src/utils/services/transferUSDC.js
+++ src/utils/services/transferUSDC.js
@@ -1,13 +1,14 @@
 import { ethers } from "ethers";
+import { usdcABI } from "../contract_abi/usdcABi";
 import { notify } from "./notification";
+import { getAddress } from "./auth";
+import { useWeb3Transfer, useMoralis } from "react-moralis";
 
 const provider = new ethers.providers.Web3Provider(window.ethereum, "any");
 
 const usdc = {
-  address: "0x68ec573C119826db2eaEA1Efbfc2970cDaC869c4",
+  address: "0x04068DA6C83AFCFA0e13ba15A6696662335D5B75",
   abi: [
-    "function name() view returns (string)",
-    "function symbol() view returns (string)",
     "function gimmeSome() external",
     "function balanceOf(address _owner) public view returns (uint256 balance)",
     "function transfer(address _to, uint256 _value) public returns (bool success)",
@@ -15,56 +16,79 @@ const usdc = {
 };
 
 export async function transferUsdc() {
-  console.log("inside");
-  let receiver = "0x68ec573C119826db2eaEA1Efbfc2970cDaC869c4";
-  let amount = "1";
-  let response;
+  SendTransaction();
+  // let receiver = "";
+  // let amount = "1";
+  // let response;
 
-  await provider.send("eth_requestAccounts", []);
-  const signer = provider.getSigner();
-  let userAddress = await signer.getAddress();
+  // await provider.send("eth_requestAccounts", []);
+  // const signer = provider.getSigner();
+  // let userAddress = await signer.getAddress();
 
-  const usdcContract = new ethers.Contract(usdc.address, usdc.abi, signer);
+  // const usdcContract = new ethers.Contract(usdc.address, usdc.abi, signer);
 
-  try {
-    receiver = ethers.utils.getAddress(receiver);
-  } catch {
-    response = `Invalid address: ${receiver}`;
-    notify(response, "error");
-  }
+  // try {
+  //   receiver = ethers.utils.getAddress(receiver);
+  // } catch {
+  //   response = `Invalid address: ${receiver}`;
+  // }
 
-  try {
-    amount = ethers.utils.parseUnits(amount, 6);
-    if (amount.isNegative()) {
-      throw new Error();
-    }
-  } catch {
-    response = `Invalid amount: ${amount}`;
-    notify(response, "error");
-  }
-
-  //   const balance = await usdcContract.balanceOf(userAddress);
-
-  //   if (balance.lt(amount)) {
-  //     let amountFormatted = ethers.utils.formatUnits(amount, 6);
-  //     let balanceFormatted = ethers.utils.formatUnits(balance, 6);
-  //     console.error(
-  //       `Insufficient balance receiver send ${amountFormatted} (You have ${balanceFormatted})`
-  //     );
-
-  //     response = `Insufficient balance receiver send ${amountFormatted} (You have ${balanceFormatted})`;
-  //     notify(response, "error");
+  // try {
+  //   amount = ethers.utils.parseUnits(amount, 6);
+  //   if (amount.isNegative()) {
+  //     throw new Error();
   //   }
-  let amountFormatted = ethers.utils.formatUnits(amount, 6);
+  // } catch {
+  //   console.error(`Invalid amount: ${amount}`);
+  //   response = `Invalid amount: ${amount}`;
+  // }
 
-  response = `Transferring ${amountFormatted} USDC receiver ${receiver.slice(
-    0,
-    6
-  )}...`;
-  notify(response, "success");
+  // let amountFormatted = ethers.utils.formatUnits(amount, 6);
 
-  const tx = await usdcContract.transfer(receiver, amount, { gasPrice: 20e9 });
-  const receipt = await tx.wait();
-  response = `Transaction confirmed in block ${receipt.blockNumber}`;
-  notify(response, "success");
+  // response = `Transferring ${amountFormatted} USDC receiver ${receiver.slice(
+  //   0,
+  //   6
+  // )}...`;
+
+  // const tx = await usdcContract.transfer(receiver, amount, { gasPrice: 20e9 });
+
+  // const receipt = await tx.wait();
 }
+
+export async function SendTransaction() {
+  const send_abi = usdcABI;
+  const provider = new ethers.providers.Web3Provider(window.ethereum);
+  const signer = provider.getSigner();
+  const contract = new ethers.Contract(
+    "0x04068da6c83afcfa0e13ba15a6696662335d5b75",
+    send_abi,
+    signer
+  );
+  const send_token_amount = "0.001";
+  const numberOfTokens = ethers.utils.parseUnits(send_token_amount, 18);
+
+  try {
+    const txResponse = await contract.transfer(
+      "0xC88C729Ef2c18baf1074EA0Df537d61a54A8CE7b",
+      numberOfTokens
+    );
+    const txReceipt = await txResponse.wait();
+    console.log(txReceipt);
+  } catch (error) {
+    console.error(error.code);
+  }
+}
+
+async function getCurrentBlock() {
+  let currentBlock = await provider.getBlockNumber();
+  console.log(currentBlock);
+  return currentBlock;
+}
+
+async function getBalance(wallet) {
+  let balance = await provider.getBalance(wallet);
+  // we use the code below to convert the balance from wei to eth
+  balance = ethers.utils.formatEther(balance);
+  console.log(balance);
+}
+
