db94e46d55a697077e5de08324f39ef6c8e262f8
Arpit <arpitbindal96@gmail.com>
2022-06-21 02:38:58 +0530

    web3auth core integration

--- src/containers/LoginDialog/LoginDialog.jsx
+++ src/containers/LoginDialog/LoginDialog.jsx
@@ -1,30 +1,52 @@
 import { DialogContent, DialogContentText, DialogTitle } from '@material-ui/core'
 import React from 'react'
+import { AiOutlineCloseCircle } from 'react-icons/ai'
 import './LoginDialog.scss'
+import { Web3AuthCore } from "@web3auth/core";
+import { CHAIN_NAMESPACES } from "@web3auth/base";
+import { currentWeb3AuthChain, getWeb3AuthChainConfig } from '../../utils/services/web3auth';
+
+let clientId = process.env.REACT_APP_WEB3AUTH_APP_ID;
+
 
 function LoginDialog() {
+    const options = {
+        chainConfig: getWeb3AuthChainConfig(currentWeb3AuthChain),
+        authMode: "DAPP",
+        clientId: clientId,
+    }
+    const web3auth = new Web3AuthCore(options);
+
+
     return (
         <div className="LoginDialog">
-            <DialogContent>
-                <p>Login To Lighthouse</p>
-                <DialogContentText>
-                    <div className="email">
-                        <label htmlFor="">
-                            Using an Email
-                        </label>
-                        <input type="text" />
-                        <button className="loginBtn">Continue with email</button>
+            <div className="LoginDialog__content">
+                <DialogContent>
+                    <div className="title">
+                        <p >Login To Lighthouse</p>
+                        <AiOutlineCloseCircle />
                     </div>
 
-                    <div className="directLogin">
-                        <p className="label">Or Using one of our following</p>
-                        <button className="loginBtn">Continue with Web3Auth</button>
-                        <button className="loginBtn">Continue with Github</button>
-                        <button className="loginBtn">Continue with Google</button>
-                    </div>
-                </DialogContentText>
-            </DialogContent>
+                    <div className="content">
+                        <div className="email">
+                            <label htmlFor="">
+                                Using an Email
+                            </label>
+                            <input type="text" />
+                            <button className="fillBtn__blue">Continue with email</button>
+                        </div>
 
+                        <div className="directLogin">
+                            <label htmlFor="">
+                                Or Using one of our following
+                            </label>
+                            <button className="fillBtn__blue">Continue with Web3Auth</button>
+                            <button className="fillBtn__blue">Continue with Github</button>
+                            <button className="fillBtn__blue">Continue with Google</button>
+                        </div>
+                    </div>
+                </DialogContent>
+            </div>
         </div>
     )
 }
